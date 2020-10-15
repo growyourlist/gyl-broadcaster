991638936aa1a089d4709d9fef2f72cbbd796a0a
gyl-dev <gyl-dev@posteo.eu>
2020-10-15 15:31:09 +0200

    Pass on new sub filtering props to count

--- src/gyl-subscriber-counter.js
+++ src/gyl-subscriber-counter.js
@@ -1,82 +1,89 @@
-require('dotenv').config()
-const dynamodb = require('dynopromise-client')
-const getSubscribers = require('./getSubscribers')
-const dbTablePrefix = process.env.DB_TABLE_PREFIX || ''
+require('dotenv').config();
+const dynamodb = require('dynopromise-client');
+const getSubscribers = require('./getSubscribers');
+const dbTablePrefix = process.env.DB_TABLE_PREFIX || '';
 
 // Create DB connection
 const dbParams = {
 	region: process.env.AWS_REGION,
-}
+};
 if (process.env.DYNAMODB_ENDPOINT) {
-	dbParams.endpoint = process.env.DYNAMODB_ENDPOINT
+	dbParams.endpoint = process.env.DYNAMODB_ENDPOINT;
+} else {
+	dbParams.accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+	dbParams.secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 }
-else {
-	dbParams.accessKeyId = process.env.AWS_ACCESS_KEY_ID
-	dbParams.secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
-}
-const db = dynamodb(dbParams)
+const db = dynamodb(dbParams);
 
 /**
  * Updates the `previewSubscriberCount` value in the database.
- * @param {number} countValue 
+ * @param {number} countValue
  */
-const updateCountValue = countValue => db.put({
-	TableName: `${dbTablePrefix}Settings`,
-	Item: {
-		settingName: 'previewSubscriberCount',
-		value: countValue,
-	}
-})
-.catch(err => console.error(err))
+const updateCountValue = (countValue) =>
+	db
+		.put({
+			TableName: `${dbTablePrefix}Settings`,
+			Item: {
+				settingName: 'previewSubscriberCount',
+				value: countValue,
+			},
+		})
+		.catch((err) => console.error(err));
 
 /**
  * Count the subscribers matching the filters given in the opts.
  * opts.tags string[] tags that the subscribers should have.
  * opts.properties object property key/value pairs that the subscribers should have.
- * opts.interactions object[] 
+ * opts.interactions object[]
  * @param {object} opts
  * @returns {Promise}
  */
 const countSubscribers = async (opts = {}) => {
-	let count = 0
+	let count = 0;
+	const fullOpts = {
+		tags: opts.tags || [],
+		excludeTags: opts.excludeTags || [],
+		properties: opts.properties || {},
+		interactions: opts.interactions || [],
+		interactionWithAnyEmail: opts.interactionWithAnyEmail,
+		ignoreConfirmed: opts.ignoreConfirmed,
+	};
 
 	// Use an interval to update the count value in the database every second.
-	let intervalId = setInterval(() => updateCountValue({
-		count,
-		status: 'processing'
-	}), 1000)
+	let intervalId = setInterval(
+		() =>
+			updateCountValue({
+				count,
+				status: 'processing',
+			}),
+		1000
+	);
 
 	try {
-
 		// Get all subscribers matching the filters
-		const subscribers = await getSubscribers({
-			consistentRead: false,
-			tags: opts.tags || [],
-			excludeTags: opts.excludeTags || [],
-			properties: opts.properties || {},
-			interactions: opts.interactions || [],
-			onEachSubscriber: () => ++count,
-		})
-		clearInterval(intervalId)
+		const subscribers = await getSubscribers(
+			Object.assign({}, fullOpts, {
+				consistentRead: false,
+				onEachSubscriber: () => ++count,
+			})
+		);
+		clearInterval(intervalId);
 
 		// Return the final subscriber count.
-		await updateCountValue({
+		await updateCountValue(Object.assign({}, fullOpts, {
 			count: subscribers.length,
 			status: 'complete',
-			tags: opts.tags,
-		})
-	}
-	catch (err) {
-
+		}));
+	} catch (err) {
 		// Update the status of the count with the error.
-		clearInterval(intervalId)
-		await updateCountValue({
+		console.error(err)
+		clearInterval(intervalId);
+		await updateCountValue(Object.assign({}, fullOpts, {
 			count,
 			status: 'error',
 			errorMessage: err.message,
-			tags: opts.tags
-		})
+		}));
 	}
-}
+};
 
-module.exports = countSubscribers
+module.exports = countSubscribers;
